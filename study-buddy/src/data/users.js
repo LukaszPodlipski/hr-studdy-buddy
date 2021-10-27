564a5f54cd93edd5e47665485c0c363bbe218e7a
LukaszPodlipski <lukaszpodlipskikontakt@gmail.com>
2021-10-27 20:48:17 +0200

    made userslist component

---
+++ study-buddy/src/data/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    avarage: '2.3',
+  },
+  {
+    name: 'Anna Pasut',
+    attendance: '59%',
+    avarage: '3.1',
+  },
+  {
+    name: 'Konrad Zbaniuk',
+    attendance: '79%',
+    avarage: '4.6',
+  },
+  {
+    name: 'Angelika Socha',
+    attendance: '2%',
+    avarage: '2.1',
+  },
+  {
+    name: 'Mariusz Murawski',
+    attendance: '45%',
+    avarage: '3.9',
+  },
+  {
+    name: 'Maria Kuśka',
+    attendance: '89%',
+    avarage: '5.0',
+  },
+];
