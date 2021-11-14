1261c77ef1774c79410cf3c5f196fe4261ae8067
Łukasz Podlipski <73656796+LukaszPodlipski@users.noreply.github.com>
2021-11-14 16:53:18 +0100

    Delete 02 directory

--- 02/dist/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var e,a;e=document.querySelector("#age"),a=[{name:"Adam",age:28},{name:"Ania",age:35},{name:"Roman",age:18}],e.innerHTML=a.reduce((function(e,a){return e+a.age}),0)})();
--- 02/src/index.js
+++
@@ -1,6 +0,0 @@
-import { data } from './data.js';
-import { combineAge, renderAge } from './helpers.js';
-
-console.log(combineAge(data));
-const container = document.querySelector('#age');
-renderAge(container, data);
