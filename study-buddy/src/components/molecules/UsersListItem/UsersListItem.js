c2cd73a0038a89c09e35694116ad5c73b2ded4b5
LukaszPodlipski <lukaszpodlipskikontakt@gmail.com>
2021-11-14 16:05:29 +0100

    routing

--- study-buddy/src/components/molecules/UsersListItem/UsersListItem.js
+++ study-buddy/src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'components/atoms/button/button';
+import DeleteButton from 'components/atoms/deleteButton/deleteButton';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 
-const UsersListItem = ({ deleteUser, index, userData: { average, name, attendance = '0%' } }) => (
+const UsersListItem = ({ deleteUser, userData: { average, name, attendance = '0%' } }) => (
   <Wrapper>
     <StyledAverage value={average}>{average}</StyledAverage>
     <StyledInfo>
       <p>
         {name}
-        <Button onClick={() => deleteUser(name)} />
+        <DeleteButton onClick={() => deleteUser(name)} />
       </p>
       <p>attendance: {attendance}</p>
     </StyledInfo>
